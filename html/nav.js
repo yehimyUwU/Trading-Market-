9f8d2dcad90a7551884bdd53996753b5c5afe2e6
CristianDanielAguilar999 <cristianmolano415@gmail.com>
2025-03-26 10:10:55 -0500

    js

--- html/nav.js
+++ html/nav.js
@@ -266,30 +266,19 @@ document.addEventListener("DOMContentLoaded", function () {
                             <h5 class="card-title">${producto.nombre}</h5>
                             <p class="card-text">${producto.descripcion}</p>
                             <p class="card-price">$${producto.precio}</p>
-                            <button class="btn btn-primary agregar-carrito mb-3" data-id="${producto.id_producto}" data-nombre="${producto.nombre}" data-precio="${producto.precio}">Agregar al carrito</button>
+                            <button class="btn btn-primary agregar-carrito mb-3" data-id="${producto.id_producto}">Agregar al carrito</button>
+                            <button class="btn btn-info ver-detalles" data-id="${producto.id_producto}">Ver detalles</button>
                         </div>
                     </div>
                 </div>
             `;
             contenedor.innerHTML += productoHTML;
         });
-<<<<<<< HEAD
-    
-        // Reasignar eventos a los botones "Agregar al carrito"
-        document.querySelectorAll(".agregar-carrito").forEach(boton => {
-            boton.addEventListener("click", function () {
-                const id = this.getAttribute("data-id");
-                const nombre = this.getAttribute("data-nombre");
-                const precio = this.getAttribute("data-precio");
-    
-                agregarAlCarrito(id, nombre, precio);
-=======
 
         document.querySelectorAll(".agregar-carrito").forEach(btn => {
             btn.addEventListener("click", function () {
                 let idProducto = this.getAttribute("data-id");
                 agregarAlCarrito(idProducto);
->>>>>>> c3c359a25b88d7b86d9daf9568c09bca6cf20d9a
             });
         });
 
@@ -312,9 +301,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         $("#productoModal").modal("show");
     }
-    
-        
-  
 
     function filtrarProductos() {
         let productosFiltrados = [...productosOriginales];
