8a8a0b543455fc281e2a603b028535317af9d36e
CristianDanielAguilar999 <cristianmolano415@gmail.com>
2025-03-27 08:09:24 -0500

    arreglo 2

--- html/nav.js
+++ html/nav.js
@@ -156,7 +156,7 @@ function calcularTotalCarrito() {
     const total = carrito.reduce((sum, producto) => sum + producto.precio * producto.cantidad, 0);
     document.getElementById("totalCarrito").innerText = `$${total.toFixed(2)}`;
 }
-bb 
+
 
 // Función para agregar productos al carritoSDFGSDFG
 function agregarAlCarrito(id, nombre, precio) {
